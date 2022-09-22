bd261a874d79aee3ebe04f07fbe0f3a6fe43eb88
Jamesmed1999 <74335705+Jamesmed1999@users.noreply.github.com>
2022-09-21 22:52:44 -0400

    inital commit

---
+++ src/Components/Intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./intro.css"
+function Intro() {
+    return (
+      <div className="intro">
+        <header className="intro">
+          <p id = "test"> James<br></br>Medina </p>
+          <p id = 'test2'> I got vs code to work <br></br>now the real magic begins</p>
+        </header>
+      </div>
+
+
+    );
+  }
+  
+  export default Intro;
